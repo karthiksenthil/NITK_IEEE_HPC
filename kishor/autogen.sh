c803176ddd3031e8950d25f523a5f1e1ff5dcc67
Kishor Bhat <kishorbhat@gmail.com>
2015-03-27 18:16:34 +0530

    added combined plotter

--- kishor/autogen.sh
+++ kishor/autogen.sh
@@ -17,6 +17,22 @@ then
 		./main.app $i >> main_vals.txt
 	done
 	python grapher.py seq
+elif [ $1 == "both" ]
+then
+	> parallel_vals.txt
+	for i in 100 200 300 400 500
+	do
+		echo $(($i * 2)) >> parallel_vals.txt
+		./main_parallel.app $i >> parallel_vals.txt
+	done
+
+	> main_vals.txt
+	for i in 100 200 300 400 500
+	do
+		echo $(($i * 2)) >> main_vals.txt
+		./main.app $i >> main_vals.txt
+	done
+	python grapher.py both
 else
-	echo "Usage: ./getvals.sh [seq|par]"
+	echo "Usage: ./autogen.sh [seq|par]"
 fi
